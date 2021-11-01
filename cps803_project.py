b04250e70bddc4a046d3747a6bddb2712e857c09
Elwad <61798510+Elwad@users.noreply.github.com>
2021-11-01 13:44:28 -0400

    Add files via upload

---
+++ cps803_project.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  1 13:35:15 2021
+
+@author: elwadgedleh
+"""
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+
+heart_data = pd.read_csv("/Users/elwadgedleh/Downloads/heart.csv")
+heart_data = heart_data.drop('restecg', 1)
+#heart_data = heart_data[heart_data.sex != 1]
+x_train = heart_data.iloc[:,0:9]
+print(x_train)
