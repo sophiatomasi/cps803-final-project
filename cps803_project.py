a17d9a9f15d9906d2fa8662468b9245e969e9a1f
Elwad <61798510+Elwad@users.noreply.github.com>
2021-11-08 13:42:30 -0500

    Update cps803_project.py

--- cps803_project.py
+++ cps803_project.py
@@ -8,10 +8,25 @@ Created on Mon Nov  1 13:35:15 2021
 """
 import pandas as pd 
 import matplotlib.pyplot as plt
-
+from sklearn.model_selection import train_test_split
 
 heart_data = pd.read_csv("/Users/elwadgedleh/Downloads/heart.csv")
 heart_data = heart_data.drop('restecg', 1)
-#heart_data = heart_data[heart_data.sex != 1]
-x_train = heart_data.iloc[:,0:9]
+heart_data = heart_data.drop('slp',1)
+heart_data = heart_data.drop('caa',1)
+heart_data = heart_data.drop('thall',1)
+print(heart_data)
+
+train_data, test_data = train_test_split(heart_data, test_size=0.2, random_state=42, shuffle=True)
+
+x_train = train_data.iloc[:,0:9]
+y_train = train_data.iloc[:,9]
+
 print(x_train)
+print(y_train)
+
+x_test = test_data.iloc[:,0:9]
+y_test = test_data.iloc[:,9]
+
+print(x_test)
+print(y_test)
